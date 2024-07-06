87d79417a6b7743a6ca05d3ff81223791d425e57
AnghelKupo <shawtybrave.lexus@gmail.com>
2024-07-06 18:00:44 -0500

    prueba

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
-    fontFamily: {
-      sans: ["Montserrat", sans-serif]
-    },
-
     extend: {},
   },
   plugins: [
