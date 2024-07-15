3ded11cce189e5dafa13472ebfc52d018756c704
Alfredo Cadogan <Adrian507gonza@gmail.com>
2024-07-15 16:15:29 -0500

    modified:   pages/balancegeneral.html 	modified:   scripts/balancegeneral.js

--- scripts/balancegeneral.js
+++ scripts/balancegeneral.js
@@ -7,6 +7,7 @@ const inventariodata = obtenerLocalStorage('inventario');
 const ventasdata = obtenerLocalStorage('ventas');
 const activosdata = obtenerLocalStorage('activos');
 
+    //Activo
 
     export const calcularInventario = () => {
         let inventarioTotal = 0;
@@ -18,7 +19,7 @@ const activosdata = obtenerLocalStorage('activos');
                 cantidadVendida += ventasdata.cantidad;
             }
         });
-            console.log(cantidadVendida);
+            
     
         // Sumar los costos de todos los productos
         inventariodata.forEach(inventariodata => {
@@ -27,7 +28,7 @@ const activosdata = obtenerLocalStorage('activos');
             }
         });
 
-        console.log(inventarioIT);
+        
         
         inventarioTotal = inventarioIT - cantidadVendida;
 
@@ -49,23 +50,71 @@ const activosdata = obtenerLocalStorage('activos');
         const iv = calcularInventario();
         const cpc = CalcularCuentasPorCobrar();
 
-        totalActivosCorrientes = iv + cpc + 5000;
+        totalActivosCorrientes = iv + cpc + 15000;
     
         return totalActivosCorrientes;
     };
 
-
-    const CalcularActivosFijos = () => {
-        let totalActivosFijos = 0;
-        let totalAFN = 0;
+    const calcularMobuliario = () => {
+        let mobiliario = 0;
+        let mobiliarioDep1 = 0;
+        let mobiliarioDep2 = 0;
 
         activosdata.forEach(activosdata => {
-            if (activosdata && activosdata.costo) {
-                totalActivosFijos += activosdata.costo;
+            if (activosdata && activosdata.descripcion === 'Mobiliario') {	
+                mobiliario = activosdata.costo;
             }
         });
 
-        totalAFN = totalActivosFijos - 32000;
+        mobiliarioDep1 = mobiliario - (mobiliario * 0.1);
+
+        mobiliarioDep2 = mobiliarioDep1 - (mobiliarioDep1 * 0.1);
+
+        return mobiliarioDep2;
+
+    }
+
+    const calcularTransporte = () => {
+        let transporte = 0;
+        let transporteDep1 = 0;
+        let transporteDep2 = 0;
+
+        activosdata.forEach(activosdata => {
+            if (activosdata && activosdata.descripcion === 'Vehiculo') {	
+                transporte = activosdata.costo;
+            }
+        });
+
+        transporteDep1 = transporte - (transporte * 0.1);
+
+        transporteDep2 = transporteDep1 - (transporteDep1 * 0.1);
+
+        return transporteDep2;
+
+    }
+
+    const calcularEquipo = () => {
+        let equipo = 0;
+        let equipoDep1 = 0;
+        let equipoDep2 = 0;
+
+        activosdata.forEach(activosdata => {
+            if (activosdata && activosdata.descripcion === 'Equipos de oficina') {	
+                equipo = activosdata.costo;
+            }
+        });
+
+        equipoDep1 = equipo - (equipo * 0.1);
+
+        equipoDep2 = equipoDep1 - (equipoDep1 * 0.1);
+
+        return equipoDep2;
+    }
+
+    const CalcularActivosFijos = () => {
+        let totalAFN = 0;
+
+        totalAFN = calcularMobuliario() + calcularTransporte() + calcularEquipo() - 28000;
 
         return totalAFN;
     }
@@ -77,6 +126,9 @@ const activosdata = obtenerLocalStorage('activos');
 
         return totalActivos;
     }
+
+
+    //pasivo
 
     export const CalcularCuentasPorPagar = () => {
         let cuentas_por_pagar = 0;
@@ -99,6 +151,27 @@ const activosdata = obtenerLocalStorage('activos');
         return cuentas_por_pagar;
     };
 
+
+    const calcularPasivosCorrientesT = () => {
+        let totalPasivosCorrientes = 0;
+
+        const cp = CalcularCuentasPorPagar();
+
+        totalPasivosCorrientes = cp + 5823.72;
+
+        return totalPasivosCorrientes;
+    }
+
+    const calcularTotalPasivos = () => {
+        let totalPasivos = 0;
+
+        totalPasivos = calcularPasivosCorrientesT() + 30859.43;
+
+        return totalPasivos;
+    }
+
+    //Capital
+
     const calcularInversionI = () => {
         let inversionI = 0;
 
@@ -114,34 +187,69 @@ const activosdata = obtenerLocalStorage('activos');
     const calcularTotalCapital = () => {
         let totalCapital = 0;
 
-        totalCapital = CalcularTotalActivos() - 25560.00;
+        totalCapital = CalcularTotalActivos() - calcularTotalPasivos();
 
         return totalCapital;
-    } 
+    }
+    
+    //Balance General Proforma
 
-   
+
+    const calcularCxPp = () => {
+        let cxpp = 0;
+        let porcentajeCxPp = 0;
+
+        cxpp = (1/24) * 119720.00;
+
+        return cxpp
+
+    }
+
+    const calcularACP = () => {
+        let acp = 0;
+
+        acp = 15000 + 1032692.00 + calcularCxPp();
+
+        return acp
+    }
+
+    
 
 // Función para mostrar los valores en el HTML
 const mostrarValoresEnHTML = () => {
     const inventario = calcularInventario();
     const cuentas_por_cobrar = CalcularCuentasPorCobrar();
     const total_activos_corrientes = CalcularActivosCorrientes();
+    const mobiliario = calcularMobuliario();
+    const transporte = calcularTransporte();
+    const equipo = calcularEquipo();
     const total_activos_fijos = CalcularActivosFijos();
     const total_activos = CalcularTotalActivos();
     const cuentas_por_pagar = CalcularCuentasPorPagar();
+    const pasivos_corrientes = calcularPasivosCorrientesT();
+    const total_pasivos = calcularTotalPasivos();
     const inversion_I = calcularInversionI();
     const total_capital = calcularTotalCapital();
+    const cxpp = calcularCxPp();
+    const acp = calcularACP();
 
     // Obtener los elementos del HTML
     
     const elementoInventario = document.getElementById('valor-inventario');
     const elementoCuentasPorCobrar = document.getElementById('valor-cuentas-por-cobrar');
     const elementoActivosCorrientes = document.getElementById('valor-activos-corrientes');
+    const elementoMobiliario = document.getElementById('valor-mobiliario');
+    const elementoTransporte = document.getElementById('valor-transporte');
+    const elementoEquipo = document.getElementById('valor-equipo');
     const elementoActivosFijos = document.getElementById('valor-activos-fijos');
     const elementoTotalActivos = document.getElementById('valor-total-activos');
     const elementoCuentasPorPagar = document.getElementById('valor-cuentas-por-pagar');
+    const elementoPasivosCorrientes = document.getElementById('valor-pasivos-corrientes');
+    const elementoTotalPasivos = document.getElementById('valor-total-pasivos');
     const elementoInversionI = document.getElementById('valor-inversion-i');
     const elementoTotalCapital = document.getElementById('valor-total-capital');
+    const elementoCxpp = document.getElementById('valor-cxpp');
+    const elementoAcp = document.getElementById('valor-acp');
     
 
     // Mostrar los valores en el HTML
@@ -149,11 +257,20 @@ const mostrarValoresEnHTML = () => {
     elementoInventario.textContent = `$ ${inventario.toFixed(2)}`;
     elementoCuentasPorCobrar.textContent = `$ ${cuentas_por_cobrar.toFixed(2)}`;
     elementoActivosCorrientes.textContent = `$ ${total_activos_corrientes.toFixed(2)}`;
+    elementoMobiliario.textContent = `$ ${mobiliario.toFixed(2)}`;
+    elementoTransporte.textContent = `$ ${transporte.toFixed(2)}`;
+    elementoEquipo.textContent = `$ ${equipo.toFixed(2)}`;
     elementoActivosFijos.textContent = `$ ${total_activos_fijos.toFixed(2)}`;
     elementoTotalActivos.textContent = `$ ${total_activos.toFixed(2)}`;
     elementoCuentasPorPagar.textContent = `$ ${cuentas_por_pagar.toFixed(2)}`;
+    elementoPasivosCorrientes.textContent = `$ ${pasivos_corrientes.toFixed(2)}`;
+    elementoTotalPasivos.textContent = `$ ${total_pasivos.toFixed(2)}`;
     elementoInversionI.textContent = `$ ${inversion_I.toFixed(2)}`;
     elementoTotalCapital.textContent = `$ ${total_capital.toFixed(2)}`;
+    elementoCxpp.textContent = `$ ${cxpp.toFixed(2)}`;
+    elementoAcp.textContent = `$ ${acp.toFixed(2)}`;
+    
+    
 };
 
 // Ejecutar la función para mostrar los valores cuando se cargue la página
